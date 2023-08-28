0c550ae70ef707b1c0613b947aac842439f3eaeb
Raniery Alves Vasconcelos <58216614+RanieryAV@users.noreply.github.com>
2023-08-28 15:18:43 -0300

    Update lab.mjs (arquivos .txt mais descritivos e modularizados)
    Seguem as mudanças que fiz localmente no meu computador para deixar os .txt um pouco mais detalhados e modularizados (sem ter que ficar alterando o valor buscado diretamente nas chamadas das funções)

--- lab.mjs
+++ lab.mjs
@@ -8,26 +8,27 @@ import quadraticSearch from "./src/quadraticSearch.mjs";
 import ternarySearch from "./src/ternarySearch.mjs";
 
 const sizeInstance = 5000000
-const note = `Busca Quadrática - intância de ${sizeInstance} ordenada`
+const desiredValue=1471442
+const note = `Busca Quadrática - intância de ${sizeInstance} ordenada - valor buscado '${desiredValue}'`
 
 loadData(`./data/ordered/${sizeInstance}.txt`).then(data =>{
 
     function algorithm(){
 
         console.log("\n Busca Quadrática____________")
-        console.log(quadraticSearch(1471442, data))
+        console.log("Value '"+ desiredValue + "' found at position: " + quadraticSearch(desiredValue, data))
 
         // console.log("\n Busca Cubica____________")
-        // console.log(cubicSearch(48, data))
+        // console.log("Value '"+ desiredValue + "' found at position: " + cubicSearch(48, data))
  
         // console.log("\n Busca Sequencial____________")
-        // console.log(sequentialSearch_v1(49, data))
+        // console.log("Value '"+ desiredValue + "' found at position: " + sequentialSearch_v1(49, data))
         
         // console.log("\n Busca Binaria____________")
-        // console.log(binarySearch(48, data, 0, data.length))
+        // console.log("Value '"+ desiredValue + "' found at position: " + binarySearch(48, data, 0, data.length))
 
         // console.log("\n Busca Ternaria____________")
-        // console.log(ternarySearch(48, data))
+        // console.log("Value '"+ desiredValue + "' found at position: " + ternarySearch(48, data))
 
     }
 
