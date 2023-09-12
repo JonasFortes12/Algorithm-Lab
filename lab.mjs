76fc81b2d6419c2c479ac9f9b9b6c17dbd05907d
JonasFortes12 <jonascforte@hotmail.com>
2023-09-12 11:42:33 -0300

    chore: Update lab code

--- lab.mjs
+++ lab.mjs
@@ -1,12 +1,12 @@
 import measureComplexity from "./modules/measureIt.mjs";
-import loadData from "./modules/loadData.mjs";
+// import loadData from "./modules/loadData.mjs";
 import { generateOrderedNumbers, generateUnorderedNumbers } from "./modules/dataGenerator.mjs"; 
 import { maxVal1, maxVal2 } from "./src/highestValueSearch.mjs";
 
 // const targetValue = 1
-const sizeInstance = 102000000
-const iterations = 20
-const note = `maxVal01_${sizeInstance}`
+const sizeInstance = 10000000
+const iterations = 100
+const note = `maxVal02_${sizeInstance}`
 
 // loadData(`./data/data-for-search/ordered/${sizeInstance}.txt`).then(data =>{
 // })
