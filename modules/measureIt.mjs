5f929237d0b3e93207acb8f508c97bab6b9bd76f
JonasFortes12 <jonascforte@hotmail.com>
2023-09-12 11:35:19 -0300

    fix: resolve the memory measure problem

--- modules/measureIt.mjs
+++ modules/measureIt.mjs
@@ -2,23 +2,21 @@ import reportGenerator from "./reportGenerator.mjs";
 
 export default function measureComplexity(code, repeat = 1, description) {
   let fullTime = 0;
-  let fullMemory = 0;
+  let allMemory = []
 
   for (let i = 1; i <= repeat; i++) {
     const startTime = performance.now();
-    const startMemory = process.memoryUsage().heapUsed;
 
     code();
-
-    const endMemory = process.memoryUsage().heapUsed;
+  
     const endTime = performance.now();
-
+    
     fullTime += endTime - startTime
-    fullMemory += endMemory - startMemory;
+    allMemory.push(process.memoryUsage().heapTotal)
   }
 
   const averageTime = (fullTime/repeat).toFixed(5) // in miliseconds 
-  const averageMemory = (fullMemory / repeat).toFixed(2); // in bytes
+  const memoryPeak = Math.max(...allMemory)
 
   reportGenerator(`
   ________________${description}_________________
@@ -26,10 +24,9 @@ export default function measureComplexity(code, repeat = 1, description) {
 
   Iterações: ${repeat}
   Tempo Total: ${fullTime.toFixed(5)} milisegundos | ${(fullTime/60000).toFixed(5)} minutos 
-  Memória Total: ${fullMemory.toFixed(2)} bytes
+  Memória Pico: ${memoryPeak} bytes
 
   Tempo médio: ${averageTime} milisegundos | ${(averageTime/60000).toFixed(5)} minutos
-  Memória média: ${averageMemory} bytes
 
   `, description)
 
